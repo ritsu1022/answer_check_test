47d9cd03366fed1238f8c3947c7d33230d7ca997
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-11-28 23:46:54 +0900

    入力の長さを確認する

--- src/answer_check_test.c
+++ src/answer_check_test.c
@@ -10,8 +10,40 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define QSIZE 4
+
+bool chg_input_length_is_valid(const char* buf) {
+	size_t length = strlen(buf);
+
+	if (length < QSIZE) {
+		puts("入力が短すぎます");
+		return false;
+	}
+	if (length > QSIZE) {
+		puts("入力が長すぎます");
+		return false;
+	}
+	return true;
+}
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	setvbuf(stdout, NULL, _IONBF, 0);
+	char buf[10];
+	char* cr;
+	const int size = sizeof(buf);
+	fgets(buf, size, stdin);
+	cr = strchr(buf, '\n');
+	if(cr != NULL) {
+		*cr = '\0';
+	}
+	puts(buf);
+	if(chg_input_length_is_valid(buf)) {
+		puts("入力は妥当です");
+	} else {
+		puts("入力に不正があります");
+	}
 	return EXIT_SUCCESS;
 }
