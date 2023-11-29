5988c0da4a0a779f949671ae90729f0ed55d832d
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-11-29 16:33:01 +0900

    入力に使えない文字を確認する

--- src/answer_check_test.c
+++ src/answer_check_test.c
@@ -12,8 +12,32 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <ctype.h>
 
 #define QSIZE 4
+
+static const char qseeds[] = { 'R', 'G', 'B', 'Y', 'M', 'C' };
+static const int num_of_colors = sizeof(qseeds)/sizeof(qseeds[0]);
+
+bool chg_input_chars_is_valid(const char* buf) {
+	size_t length = strlen(buf);
+
+	char illegal_chars[QSIZE] = { '\0' };
+	int j = 0;
+	for (size_t i = 0; i < length; i++) {
+		char ch = toupper(buf[i]);
+		if (memchr(qseeds, ch, num_of_colors) == NULL) {
+			if (memchr(illegal_chars, buf[i],QSIZE) == NULL) {
+				illegal_chars[j++] = buf[i];
+			}
+		}
+	}
+	if (strlen(illegal_chars) > 0) {
+		printf("使えない文字\"%s\"が含まれています。\n", illegal_chars);
+		return false;
+	}
+	return true;
+}
 
 bool chg_input_length_is_valid(const char* buf) {
 	size_t length = strlen(buf);
@@ -40,7 +64,8 @@ int main(void) {
 		*cr = '\0';
 	}
 	puts(buf);
-	if(chg_input_length_is_valid(buf)) {
+	if(chg_input_length_is_valid(buf)
+		&& chg_input_chars_is_valid(buf)) {
 		puts("入力は妥当です");
 	} else {
 		puts("入力に不正があります");
